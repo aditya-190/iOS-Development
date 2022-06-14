11e66446bd5ae0dd188a3e5ed47dcce244949c25
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2022-06-14 15:54:51 +0530

    Very Basic Apps on XCode

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by adi on 14/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
